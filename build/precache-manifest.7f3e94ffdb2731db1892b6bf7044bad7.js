c27159a8e39a1d3cf52805a806adacec857acc24
mcarmonadev <permacultura@gmail.com>
2021-12-01 01:30:55 -0600

    initial upload
    locally devolped, and initally uploaded
    It is exercised from JsMastery video tutorial

---
+++ build/precache-manifest.7f3e94ffdb2731db1892b6bf7044bad7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39039bc2a806c3debf438dbb7163e6b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edcae3c7c743b9253da9",
+    "url": "/static/js/2.2050bb13.chunk.js"
+  },
+  {
+    "revision": "5df72fe91a1a156893e5",
+    "url": "/static/js/main.a771201e.chunk.js"
+  },
+  {
+    "revision": "c06b7b300d045328f31d",
+    "url": "/static/js/runtime~main.60d0dddc.js"
+  }
+]);
